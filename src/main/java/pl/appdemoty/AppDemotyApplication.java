6cdc808f09111dc7b7b3dc5b6600930c95dd5a3a
Rafał Kłosiński <klosinski.rafal@o2.pl>
2022-10-11 18:39:57 +0200

    first commit

---
+++ src/main/java/pl/appdemoty/AppDemotyApplication.java
@@ -0,0 +1,13 @@
+package pl.appdemoty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDemotyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppDemotyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/appdemoty/AppDemotyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.appdemoty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDemotyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
